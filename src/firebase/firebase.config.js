ea6d25669e00845c389456a997b0c492ecf47172
nusrat3657 <nusratjahan3657r@gmail.com>
2024-05-11 01:20:25 +0600

    project setup

---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyA9pYCPEWi1_T645czaXokbaVs-9XbpdsI",
+  authDomain: "restaurant-management-we-21b49.firebaseapp.com",
+  projectId: "restaurant-management-we-21b49",
+  storageBucket: "restaurant-management-we-21b49.appspot.com",
+  messagingSenderId: "493174848903",
+  appId: "1:493174848903:web:c7ff229b8e5762dedb2722"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
