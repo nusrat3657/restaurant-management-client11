2c843159c8ba2ced0cac50034c406e2cad46b89b
nusrat3657 <nusratjahan3657r@gmail.com>
2024-06-01 03:00:12 +0600

    updated firebse config

--- src/firebase/firebase.config.js
+++ src/firebase/firebase.config.js
@@ -4,13 +4,16 @@ import { initializeApp } from "firebase/app";
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
+
+console.log('inside firebase config',import.meta.env.VITE_PASS);
+
 const firebaseConfig = {
-  apiKey: "AIzaSyA9pYCPEWi1_T645czaXokbaVs-9XbpdsI",
-  authDomain: "restaurant-management-we-21b49.firebaseapp.com",
-  projectId: "restaurant-management-we-21b49",
-  storageBucket: "restaurant-management-we-21b49.appspot.com",
-  messagingSenderId: "493174848903",
-  appId: "1:493174848903:web:c7ff229b8e5762dedb2722"
+  apiKey:import.meta.env.VITE_APIKEY,
+  authDomain:import.meta.env.VITE_AUTHDOMAIN,
+  projectId:import.meta.env.VITE_PROJECTID,
+  storageBucket:import.meta.env.VITE_STORAGEBUCKET,
+  messagingSenderId:import.meta.env.VITE_MESSAGINGSENDERID,
+  appId:import.meta.env.VITE_APPID,
 };
 
 // Initialize Firebase
